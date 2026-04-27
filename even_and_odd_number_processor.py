8c6eef85c59bf45008df70b73324249de4a775dd
rainieljerez <rainiel.jerez@gmail.com>
2026-04-27 16:11:29 +0800

    fixed the program errors and checked if the program is working

--- even_and_odd_number_processor.py
+++ even_and_odd_number_processor.py
@@ -9,7 +9,31 @@ class EvenAndOddNumberProcesser:
         file = open('numbers.txt', 'r')
         for line in file:
             self.numbers.append(int(line.strip()))
-        file.close
+        file.close()
 #classify to even and odd numbers
+    def classify_numbers(self):
+        for num in self.numbers:
+            if num % 2 == 0:
+                self.even_numbers.append(num)
+            else:
+                self.odd_numbers.append(num)
 #method for even
+    def write_even_numbers(self):
+        file = open("even.txt", "w")
+        for num in self.even_numbers:
+            file.write(str(num) + "\n")
+        file.close()
 #method for odd
+    def write_odd_numbers(self):
+        file = open("odd.txt", "w")
+        for num in self.odd_numbers:
+            file.write(str(num) + "\n")
+        file.close()
+
+processor = EvenAndOddNumberProcesser()
+processor.read_numbers()
+processor.classify_numbers()
+processor.write_even_numbers()
+processor.write_odd_numbers()
+
+print("Done! Check even.txt and odd.txt for the results.")
