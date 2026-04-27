59c4122a04600e12c3746c145d2ff2d53fc1bf84
rainieljerez <rainiel.jerez@gmail.com>
2026-04-28 06:40:36 +0800

    stated the flow of the program

---
+++ square_of_even_and_cube_of_odd_integers.py
@@ -0,0 +1,13 @@
+
+
+class CubeAndSquareInteger:
+    def __init__(self, source_file):
+        self.source_file = source_file
+        self.integers = []
+#read file
+    def
+
+#method for square
+#method for cube
+#process for the methods
+#process the file
