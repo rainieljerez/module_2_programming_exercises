7ca3fee18c7df9728d7085b32160e2c2ec91edac
rainieljerez <rainiel.jerez@gmail.com>
2026-04-27 14:59:44 +0800

    done first part of the flow

--- even_and_odd_number_processor.py
+++ even_and_odd_number_processor.py
@@ -1,5 +1,15 @@
+class EvenAndOddNumberProcesser:
+    def __init__(self):
+        self.numbers = []
+        self.even_numbers = []
+        self.odd_numbers = []
 
 #open the file
+    def read_numbers(self):
+        file = open('numbers.txt', 'r')
+        for line in file:
+            self.numbers.append(int(line.strip()))
+        file.close
 #classify to even and odd numbers
 #method for even
 #method for odd
